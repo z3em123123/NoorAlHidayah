f7b9e2fb1c28c2ef4a2b97476e6ed4c9c0acf899
z3em123123 <ttmnz137@gmail.com>
2026-06-11 12:03:56 +0300

    feat: add color theme definitions

---
+++ app/src/main/java/com/noor/alhidayah/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.noor.alhidayah.ui.theme
+import androidx.compose.ui.graphics.Color
+val SkyBlue=Color(0xFF38BDF8);val SkyBlueDark=Color(0xFF0284C7);val SkyBlueLight=Color(0xFF7DD3FC);val SkyBlueContainer=Color(0xFF0C4A6E)
+val Emerald=Color(0xFF10B981);val EmeraldDark=Color(0xFF059669);val EmeraldLight=Color(0xFF34D399);val EmeraldContainer=Color(0xFF064E3B)
+val Gold=Color(0xFFF59E0B);val GoldDark=Color(0xFFB45309);val GoldLight=Color(0xFFFCD34D);val GoldContainer=Color(0xFF78350F)
+val NightSky=Color(0xFF0F172A);val NightSkyDark=Color(0xFF020617);val NightSkyLight=Color(0xFF1E293B);val NightSkySurface=Color(0xFF1E293B);val NightSkySurfaceVariant=Color(0xFF334155)
+val OnDarkText=Color(0xFFF1F5F9);val OnDarkTextSecondary=Color(0xFF94A3B8)
+val GlassSurface=Color(0x1AFFFFFF);val GlassBorder=Color(0x26FFFFFF);val GlassSurfaceStrong=Color(0x26FFFFFF)
+val ErrorRed=Color(0xFFEF4444);val ErrorRedContainer=Color(0xFF7F1D1D)
+val FajrColor=Color(0xFF6366F1);val DhuhrColor=Color(0xFFF59E0B);val AsrColor=Color(0xFFF97316);val MaghribColor=Color(0xFFEF4444);val IshaColor=Color(0xFF8B5CF6)
